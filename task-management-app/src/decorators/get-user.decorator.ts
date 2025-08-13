b2a037d20a387a185a87049580057a78e9b1763e
Khang Nguyen <ngkhang0220@gmail.com>
2025-08-13 21:37:23 +0700

    update: section 5 - adds @GetUser decorator

---
+++ task-management-app/src/decorators/get-user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/auth/user.entity';
+
+export const GetUser = createParamDecorator(
+  (_data, ctx: ExecutionContext): User => {
+    const req = ctx.switchToHttp().getRequest<{ user: User }>();
+
+    return req.user;
+  },
+);
